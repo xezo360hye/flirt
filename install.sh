3eaa3ece5e84ffe1e8d677052e6a2197d750fc5d
TheGhostboi64 <99986496+TheGhostboi64@users.noreply.github.com>
2022-07-26 21:29:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,23 +16,12 @@ rm -rf flirt
 
 cat << EOF >> .bashrc
 
-#!/bin/bash
-(
-color=( 31 "1;31" 33 "1;33" "1;34" "1;35" "1;36" )
-text=( "It shall be done, comrade"
-"As you wish, honey :3"
-"Executing, my love"
-"I'll do that for you, sweetheart" )
-
-sel_color="\e[${color[$RANDOM % ${#color[@]}]}m"
-sel_text="~ ${text[$RANDOM % ${#text[@]}]}"
-reset="\e[0m\n"
-
-echo -e "${sel_color}${sel_text}${reset}"
-)
+PROMPT_COMMAND="prompt=1"
+trap 'cmd=\$BASH_COMMAND; \
+[[ "\$prompt" && "\$cmd" != "\$PROMPT_COMMAND" ]] && \
+~/.flirt; unset prompt' DEBUG
 EOF
 
 chmod +x .flirt
-chmod +x .bashrc
 
 echo "Installation success, start bash again for the changes to take effect"
