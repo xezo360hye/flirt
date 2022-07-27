e7ecc173d0fc91a65220643c530feda235eef854
TheGhostboi64 <99986496+TheGhostboi64@users.noreply.github.com>
2022-07-26 21:52:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,24 @@ trap 'cmd=\$BASH_COMMAND; \
 ~/.flirt; unset prompt' DEBUG
 EOF
 
+touch uninstall-flirt.sh
+cat << EOF >> uninstall-flirt.sh
+
+#!/bin/bash
+PS3="Are you sure you want to uninstall flirt?: "
+select choice in yes no; do
+case $choice in
+yes) rm .flirt ;;
+no) exit
+
+rm .bashrc
+cat .bashrc
+echo "Successfully uninstalled, thank you for using flirt. Have a nice morning/afternoon/night!"
+		esac
+	done
+}
+EOF
+
 chmod +x .flirt
 
 echo "Installation success, start bash again for the changes to take effect.\nif you need to uninstall it, run bash uninstall-flirt.sh"
