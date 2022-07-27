d022cc7fa0a1c58fbaa3bb4dd2e2747925d2ff66
TheGhostboi64 <99986496+TheGhostboi64@users.noreply.github.com>
2022-07-26 20:56:24 -0400

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PS3="Are you sure you want to uninstall flirt?: "
+select choice in yes no; do
+case $choice in
+yes) rm .flirt ;;
+no) exit
+
+rm .bashrc
+cat .bashrc
+echo "Successfully uninstalled, thank you for using flirt. Have a nice morning/afternoon/night!"
+		esac
+	done
+}
